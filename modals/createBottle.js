4864351c6327e4c8f7c3943b702c54b509d6d526
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-10-04 21:41:06 +0200

    create bottle (example)

--- modals/createBottle.js
+++ modals/createBottle.js
@@ -1,8 +1,60 @@
+const { ChannelType, PermissionFlagsBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js");
+const { newBottleCategory } = require('../config.json');
+const createEmbeds = require('../utils/createEmbeds.js');
+
 module.exports = {
     name: 'createBottle',
     async execute(interaction) {
-        const text = interaction.fields.getTextInputValue('textBottle');
-        // TODO: edit text and sent it.
+
+        const content = interaction.fields.getTextInputValue('textBottle');
+
+        const sender = interaction.member;
+
+        // TODO: check if message is OK (moderation, sad text, injuries, ...)
+
+        // TODO: choose a channel_name
+        const channel_name = "bouteille-rose-cassée";
+
+        // TODO: create channel
+        const everyoneRole = interaction.guild.roles.everyone;
+        var channel = await interaction.guild.channels.create({
+            name: channel_name,
+            type: ChannelType.GuildText
+        })
+
+        // TODO: move channel to "nouvelles bouteilles"
+        const category = interaction.guild.channels.cache.find(c => c.id == newBottleCategory);
+        await channel.setParent(category);
+        //await channel.setPosition(0);
+
+        // TODO: choose random member
+        const members = await interaction.guild.members.fetch();
+        const randMember = members.random();
+
+        // TODO: add member to channel
+        // edits overwrites to disallow everyone to view the channel
+        await channel.permissionOverwrites.edit(interaction.guild.id, {ViewChannel: false});
+
+        // edits overwrites to allow a user to view the channel
+        await channel.permissionOverwrites.edit(randMember.id, {ViewChannel: true});
+
+        // TODO: create bottle message ...
+        const embed = createEmbeds.createBottle(content);
+
+        // ... with actions (reply, signal, resend to ocean)
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('replyBottle')
+                    .setLabel('Répondre')
+                    .setStyle(ButtonStyle.Primary),
+            );
+
+        // Send to channel
+        channel.send({ content: randMember.toString(), embeds: [embed], components: [row] });
+
+        // TODO: save it to DB
+
         await interaction.reply({ content: 'Votre bouteille a été envoyée.', ephemeral: true });
     },
 };
--- utils/roles.js
+++ utils/roles.js
@@ -3,14 +3,12 @@ const { modRole, adminRole } = require('../config.json');
 
 module.exports = {
      userIsAdmin: async function (member) {
-         //if (user.)
-             if (member.roles.cache.find(r => r.id === adminRole))
+         if (member.roles.cache.find(r => r.id === adminRole))
             return true;
          return false;
      },
 
     userIsMod: async function (member) {
-        //if (user.)
         if (member.roles.cache.find(r => r.id === modRole))
             return true;
         return false;
