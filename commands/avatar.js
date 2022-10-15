0a374f20ffbcd1fb8833875428b1f90caf633e14
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-10-15 16:51:32 +0200

    fix: update commands
    (+) avatar check if user is in db
    (+) init [type]
    (-) delete initticket

--- commands/avatar.js
+++ commands/avatar.js
@@ -20,6 +20,13 @@ module.exports = {
         const url = 'https://avatars.dicebear.com/api/adventurer-neutral/' + code + '.png'
         const embed = createEmbeds.createFullEmbed('Encore un super avatar', 'Voici votre avatar pour vos prochaines bouteilles !', null, url, null, 'Code de l\'avatar : ' + code);
 
+        // Check if the user exists in the database
+        const userId = await userDB.getUser(interaction.user.id);
+        if (userId == null) {
+            // Add the user to the database
+            await userDB.createUser(interaction.user.id, 0, 0);
+        }
+
         await userDB.update_diceBearSeed(interaction.user.id, code);
 
         return interaction.reply({ content: "", embeds: [embed], ephemeral: true });
--- commands/init.js
+++ commands/init.js
@@ -4,18 +4,64 @@ const roles = require('../utils/roles.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('init')
-        .setDescription('Initialize the #ma_bouteille message\'s'),
+        .setDescription('Initialize the #ma_bouteille message\'s')
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('The type of the init')
+                .setRequired(true)),
     async execute(interaction) {
         if (await roles.userIsAdmin(interaction.member)) {
-            const row = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('createBottle')
-                        .setLabel('Click me!')
-                        .setStyle(ButtonStyle.Primary),
-                );
-            await interaction.reply({ content:'C\'est fait.', ephemeral: true});
-            return interaction.channel.send({ content: 'I think you should,', components: [row] });
+            if (interaction.options.getString('type') === 'ticket') {
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('createTicket')
+                            .setLabel('Click me!')
+                            .setStyle(ButtonStyle.Primary),
+                    );
+                await interaction.reply({ content:'C\'est fait.', ephemeral: true});
+                return interaction.channel.send({ content: 'I think you should,', components: [row] });
+            } else if (interaction.options.getString('type') === 'bottle') {
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('createBottle')
+                            .setLabel('Click me!')
+                            .setStyle(ButtonStyle.Primary),
+                    );
+                await interaction.reply({ content:'C\'est fait.', ephemeral: true});
+                return interaction.channel.send({ content: 'I think you should,', components: [row] });
+            } else if (interaction.options.getString('type') === 'opinion') {
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('createOpinion')
+                            .setLabel('Click me!')
+                            .setStyle(ButtonStyle.Primary),
+                    );
+                await interaction.reply({ content:'C\'est fait.', ephemeral: true});
+                return interaction.channel.send({ content: 'I think you should,', components: [row] });
+            } else if (interaction.options.getString('type') === 'suggestion') {
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('createSuggestion')
+                            .setLabel('Click me!')
+                            .setStyle(ButtonStyle.Primary),
+                    );
+                await interaction.reply({ content:'C\'est fait.', ephemeral: true});
+                return interaction.channel.send({ content: 'I think you should,', components: [row] });
+            } else if (interaction.options.getString('type') === 'help') {
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('createHelp')
+                            .setLabel('Click me!')
+                            .setStyle(ButtonStyle.Primary),
+                    );
+                await interaction.reply({ content:'C\'est fait.', ephemeral: true});
+                return interaction.channel.send({ content: 'I think you should,', components: [row] });
+            }
         }
         return interaction.reply({ content:'Vous n\'avez pas le droit de faire cela.', ephemeral: true});
     },
