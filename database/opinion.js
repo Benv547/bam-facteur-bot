3aa88ac57b05357b3040139489615b83529fb591
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-10-15 16:52:49 +0200

    feat: opinion feature

---
+++ database/opinion.js
@@ -0,0 +1,33 @@
+const { dbuser, dbhost, dbbase, dbpassword, dbport } = require('../config.json');
+
+const Pool = require('pg').Pool
+function getPool() {
+    const pool = new Pool({
+        user: dbuser,
+        host: dbhost,
+        database: dbbase,
+        password: dbpassword,
+        port: dbport
+        // ssl: { rejectUnauthorized: false }
+    })
+    return pool;
+}
+
+module.exports = {
+    insertOpinion: async function (id_message, id_user, content) {
+        const pool = getPool();
+        return await pool.query('INSERT INTO "Opinion" ("id_message", "id_user", "content") VALUES ($1, $2, $3)', [id_message, id_user, content]);
+    },
+    getOpinion: async function (id_user) {
+        const pool = getPool();
+        const results = await pool.query('SELECT * FROM "Opinion" WHERE "id_user" = $1', [id_user]);
+        if (results.rows.length === 0) {
+            return null;
+        }
+        return results.rows[0]["id_message"];
+    },
+    deleteOpinion: async function (id_user) {
+        const pool = getPool();
+        return await pool.query('DELETE FROM "Opinion" WHERE "id_user" = $1', [id_user]);
+    }
+}
---
+++ modals/createOpinion.js
@@ -0,0 +1,52 @@
+const {ChannelType, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js");
+const createEmbeds = require("../utils/createEmbeds");
+const opinionDB = require("../database/opinion");
+const userDB = require("../database/user");
+
+module.exports = {
+    name: 'createOpinion',
+    async execute(interaction) {
+
+        // Check if the user exists in the database
+        const userId = await userDB.getUser(interaction.user.id);
+        if (userId == null) {
+            // Add the user to the database
+            await userDB.createUser(interaction.user.id, 0, 0);
+        }
+
+        // Check if the user dont have a opinion
+        const opinion = await opinionDB.getOpinion(interaction.user.id);
+        if (opinion !== null) {
+            try {
+                // Fetch message from database
+                const messageId = await opinionDB.getOpinion(interaction.user.id);
+
+                // Fetch message on channel
+                const message = await interaction.channel.messages.fetch(messageId);
+
+                // Delete message
+                await message.delete();
+
+                // Delete the old opinion
+                await opinionDB.deleteOpinion(interaction.user.id);
+            } catch (e) {
+                console.error(e);
+                return await interaction.reply({ content: 'Une erreur est survenue lors de la suppression de votre avis.', ephemeral: true });
+            }
+        }
+
+        // Get content
+        const content = interaction.fields.getTextInputValue('textOpinion');
+
+        // Create embed
+        const embed = createEmbeds.createFullEmbed("Un•e illustre inconnu•e", content, null, null, 0x2F3136, null);
+
+        // Send embed
+        const message = await interaction.channel.send({ content: '', embeds: [embed] });
+
+        // Save message id in database
+        await opinionDB.insertOpinion(message.id, interaction.user.id, content);
+
+        await interaction.reply({ content: 'Votre opinion a été envoyée.', ephemeral: true });
+    }
+};
