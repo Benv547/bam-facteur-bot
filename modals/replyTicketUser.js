c3c25e499557dc58790c15ee41f506c6f8c85af7
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-10-19 11:49:26 +0200

    fix: problem with ticket if deleted (user MP)

--- modals/replyTicketUser.js
+++ modals/replyTicketUser.js
@@ -10,8 +10,6 @@ module.exports = {
 
         const content = interaction.fields.getTextInputValue('textTicket');
 
-        await interaction.reply({ content: 'Votre réponse a été envoyée.', ephemeral: true });
-
         // Create a button to reply to the ticket
         const rowMod = new ActionRowBuilder()
             .addComponents(
@@ -23,15 +21,25 @@ module.exports = {
 
         // Fetch channel from database
         const channelId = await ticketDB.get_id_channel(interaction.user.id);
+        if (channelId == null) {
+            return await interaction.reply({ content: 'Votre ticket est introuvable ou a déjà été fermé. Merci d\'en créer un nouveau.', ephemeral: true });
+            return;
+        }
 
         // Fetch guild from database
         const guildId = await ticketDB.get_id_guild(interaction.user.id);
+        if (guildId == null) {
+            return await interaction.reply({ content: 'Votre ticket est introuvabl ou a déjà été fermé. Merci d\'en créer un nouveau.', ephemeral: true });
+            return;
+        }
+
+        await interaction.reply({ content: 'Votre réponse a été envoyée.', ephemeral: true });
 
         // Fetch guild from client
-        const guild = interaction.client.guilds.cache.get(guildId);
+        const guild = await interaction.client.guilds.fetch(guildId);
 
         // Fetch channel from guild
-        const channelGuild = guild.channels.cache.get(channelId);
+        const channelGuild = await guild.channels.fetch(channelId);
 
         const embedMod = createEmbeds.createFullEmbed("Un•e illuste inconnu•e", content, null, null, 0x0000FF, null);
         await channelGuild.send({ content: '', embeds: [embedMod], components: [rowMod] });
