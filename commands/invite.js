df59a23abf17883eb6cc60eac9ca76b218aeb45b
Suzoka/Morgan <mor9an77@gmail.com>
2022-10-28 14:06:34 +0200

    style: personalized message for /invite

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,23 @@ module.exports = {
     async execute(interaction) {
         // Get the user's currency
         const guest = await userDB.get_nb_invite(interaction.user.id);
-        const embed = createEmbeds.createFullEmbed('Ça fait beaucoup de monde !', 'Vous avez invité **' + guest + ' utisateurs** !', null, null, 0x2f3136, null);
-        return interaction.reply({ content: "", embeds: [embed], ephemeral:true });
+        let embed = createEmbeds.createFullEmbed('', '', null, null, 0x2f3136, null);
+
+        if (!guest) {
+            embed = createEmbeds.createFullEmbed('Vous n\'avez encore invité personne 😔', 'Vous pouvez inviter des gens...', null, null, 0x2f3136, null);
+        }
+        else if (guest == 1) {
+            embed = createEmbeds.createFullEmbed('C\'est un bon début !', 'Vous avez invité **' + guest + " personne**", null, null, 0x2f3136, null);
+        }
+        else if (guest >= 2 && guest < 5) {
+            embed = createEmbeds.createFullEmbed('Pas mal !', 'Vous avez invité **' + guest + ' utisateurs** !', null, null, 0x2f3136, null);
+        }
+        else if(guest>= 6 && guest < 10) {
+    embed = createEmbeds.createFullEmbed('Ça fait beaucoup de monde !', 'Vous avez invité **' + guest + ' utisateurs** !', null, null, 0x2f3136, null);
+}
+        else if (guest >= 10) {
+    embed = createEmbeds.createFullEmbed('Vous êtes légendaire !', 'Vous avez invité **' + guest + ' utisateurs** !\n C\'est incroyable d\'avoir autant de monde, vous êtes vraiment un grand fan de bouteille à la mer et nous vous remercions de votre fidélité ❤', null, null, 0x2f3136, null);
+}
+return interaction.reply({ content: "", embeds: [embed], ephemeral: true });
     },
 };
