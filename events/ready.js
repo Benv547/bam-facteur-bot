a62b37a5313337a527109fcc094b823f1127de9f
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-11-04 19:27:09 +0100

    fix: ile au trésors 20

--- events/ready.js
+++ events/ready.js
@@ -34,7 +34,7 @@ module.exports = {
             // if channel members length is > to 5% of the guild members length
             // not count bots and admins
             let channelSize = channel.members.filter((member) => !member.user.bot && !member.roles.cache.has(adminRole)).size;
-            let guildSize = (await guild.members.fetch()).size * 0.05;
+            let guildSize = 20;
             while (channelSize > guildSize) {
                 try {
                     // choose a non bot member in the channel
