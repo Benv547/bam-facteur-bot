9189724c4e6b92b0add4ffa187857c8c645f7fa5
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-11-06 14:35:49 +0100

    fix: double vote rewards

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -7,26 +7,32 @@ const OR_BUMP = 150;
 const XP_VOTE = 10;
 const XP_BUMP = 20;
 
+let voteId = [];
+
 module.exports = {
     name: 'messageUpdate',
-    async execute(message) {
-        if (message.reactions && message.reactions.message.embeds) {
-            const msg = message.reactions.message;
-            if (message.interaction) {
+    async execute(oldMessage, newMessage) {
+        if (newMessage.reactions && newMessage.reactions.message.embeds) {
+            const msg = newMessage.reactions.message;
+            if (newMessage.interaction) {
                 if (msg.embeds.length > 0) {
-                    const content = msg.embeds[0].description;
-                    if (content) {
-                        const user = message.interaction.user;
+                    if (content && !voteId.includes(newMessage.id)) {
+                        voteId.push(newMessage.id);
+                        const user = newMessage.interaction.user;
                         if (content.includes('a Vot')) {
                             await orAction.increment(user.id, OR_VOTE);
-                            await xpAction.increment(message.guild, user.id, XP_VOTE);
+                            await xpAction.increment(newMessage.guild, user.id, XP_VOTE);
                             const embed = createEmbeds.createFullEmbed(`Merci pour ton vote !`, user.toString()  + ', vous avez reçu **' + OR_VOTE + ' pièce(s) d\'or** et de l\'expérience.', null, null, null, null);
-                            await message.channel.send({content: "", embeds: [embed]});
+                            await newMessage.channel.send({content: "", embeds: [embed]});
                         } else if (content.includes('a BUMP')) {
                             await orAction.increment(user.id, OR_BUMP);
-                            await xpAction.increment(message.guild, user.id, XP_BUMP);
+                            await xpAction.increment(newMessage.guild, user.id, XP_BUMP);
                             const embed = createEmbeds.createFullEmbed(`Merci pour ton bump !`, user.toString()  + ', vous avez reçu **' + OR_BUMP + ' pièce(s) d\'or** et de l\'expérience.', null, null, null, null);
-                            await message.channel.send({content: "", embeds: [embed]});
+                            await newMessage.channel.send({content: "", embeds: [embed]});
+                        }
+                        if (voteId.length > 10) {
+                            // remove first item
+                            voteId.shift();
                         }
                     }
                 }
