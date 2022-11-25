4b78994b9eeed6017625877aa9c6af075a144308
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-11-25 15:23:23 +0100

    fix: /voir response ephemeral

--- commands/voir.js
+++ commands/voir.js
@@ -35,22 +35,22 @@ module.exports = {
                 const embed = createEmbeds.createFullEmbed('Sticker ' + sticker.name, null, null, sticker.url, 0x2f3136, null);
                 return await interaction.reply({content: "", embeds: [embed], ephemeral: true});
             } else {
-                return await interaction.reply('Plusieurs stickers correspondent à ce nom, veuillez préciser.');
+                return await interaction.reply({ content: 'Plusieurs stickers correspondent à ce nom, veuillez préciser.', ephemeral: true });
             }
         } else if (categorie == 'arabesque') {
             const footers = await footerDB.getFooterWithName(item);
             if (footers === null || footers.length == 0) {
-                return await interaction.reply('Aucune arabesque ne correspond à ce nom.');
+                return await interaction.reply({content: 'Aucune arabesque ne correspond à ce nom.', ephemeral: true});
             }
             if (footers.length === 1) {
                 const footer = footers[0];
                 const embed = createEmbeds.createFullEmbed('Arabesque ' + footer.name, null, null, footer.url, 0x2f3136, null);
                 return await interaction.reply({content: "", embeds: [embed], ephemeral: true});
             } else {
-                return await interaction.reply('Plusieurs arabesques correspondent à ce nom, veuillez préciser.');
+                return await interaction.reply({content: 'Plusieurs arabesques correspondent à ce nom, veuillez préciser.', ephemeral: true});
             }
         }
 
-        return await interaction.reply('Cette catégorie n\'existe pas.');
+        return await interaction.reply({ content: 'Cette catégorie n\'existe pas.', ephemeral: true });
     }
 };
