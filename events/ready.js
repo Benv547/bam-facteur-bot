1e1d5ca2a390e90a640caef6dd950e579ac06928
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-11-28 13:46:30 +0100

    fix: user username

--- events/ready.js
+++ events/ready.js
@@ -46,12 +46,17 @@ module.exports = {
 
             let guildSize = 40;
             while (usersToRemove.length > 0) {
-                const member_id = usersToRemove.pop();
-                const member = guild.members.fetch(member_id);
-                if (member !== null) {
-                    // remove permission to see the channel
-                    await channel.permissionOverwrites.delete(member_id);
-                    await channel.send(`** **\n🚣 L'illustre **${member.user.username}** a été éjecté•e de l'île !`);
+                try {
+                    const member_id = usersToRemove.pop();
+                    const member = guild.members.fetch(member_id);
+                    if (member !== null) {
+                        // remove permission to see the channel
+                        await channel.permissionOverwrites.delete(member_id);
+                        await channel.send(`** **\n🚣 L'illustre **${member.username}** a été éjecté•e de l'île !`);
+                    }
+                }
+                catch (e) {
+                    console.log(e);
                 }
             }
 
