b534c53d77224c0a39320be095da79d483e88914
suzoka <mor9an77@gmail.com>
2022-12-01 20:57:03 +0100

    style: write 1er for the classement, and not 1ème

--- commands/classement.js
+++ commands/classement.js
@@ -15,12 +15,25 @@ module.exports = {
             // Add the user to the database
             await userDB.createUser(interaction.user.id, 0, 0);
         }
-        const rankMoney = await userDB.getRankOr(interaction.user.id);
-        const rankXp = await userDB.getRankXp(interaction.user.id);
+        let rankMoney = await userDB.getRankOr(interaction.user.id);
+        let rankXp = await userDB.getRankXp(interaction.user.id);
         const money =  user.money;
         const xp =  user.xp;
+        if (rankXp == 1){
+            rankXp+="er•e"
+        }
+        else {
+            rankXp+="ème"
+        }
 
-        const embed = createEmbeds.createFullEmbed('Regardez comme vous êtes puissants !', 'Vous êtes **' + rankMoney + 'ème** au classement de l\'or avec un total de **' + money + ' pièces d\'or** ! \n\n Vous êtes **' + rankXp + 'ème** au classement de l\'XP avec un total de **' + xp + ' points d\'expérience** !', null, null, 0x2f3136, null);
+        if (rankMoney == 1){
+            rankMoney+="er•e"
+        }
+        else {
+            rankMoney+="ème"
+        }
+
+        const embed = createEmbeds.createFullEmbed('Regardez comme vous êtes puissants !', 'Vous êtes **' + rankMoney +'** au classement de l\'or avec un total de **' + money + ' pièces d\'or** ! \n\n Vous êtes **' + rankXp + '** au classement de l\'XP avec un total de **' + xp + ' points d\'expérience** !', null, null, 0x2f3136, null);
         return interaction.reply({ content: "", embeds: [embed], ephemeral:true });
     },
 };
