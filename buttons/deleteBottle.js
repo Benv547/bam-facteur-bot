8c1d53fd14239ed7f8e6dc1cbff48b95edb4aa46
Benjamin VIGNOT <benji.vignot@gmail.com>
2022-12-03 11:32:38 +0100

    feat: add raison to delete bottle

--- buttons/deleteBottle.js
+++ buttons/deleteBottle.js
@@ -10,18 +10,26 @@ module.exports = {
             .setTitle('Êtes-vous sûr ?');
 
         // Add components to modal
-        const hobbiesInput = new TextInputBuilder()
+        const deleteInput = new TextInputBuilder()
             .setCustomId('textSuppression')
             .setLabel("⚠️ Ecrivez 'supprimer' pour confirmer :")
+            // Paragraph means multiple lines of text.
+            .setStyle(TextInputStyle.Short);
+
+        const raisonInput = new TextInputBuilder()
+            .setCustomId('textRaison')
+            .setLabel("Quelle est la raison ?")
+            .setRequired(false)
             // Paragraph means multiple lines of text.
             .setStyle(TextInputStyle.Paragraph);
 
         // An action row only holds one text input,
         // so you need one action row per text input.
-        const primaryActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+        const primaryActionRow = new ActionRowBuilder().addComponents(deleteInput);
+        const secondaryActionRow = new ActionRowBuilder().addComponents(raisonInput);
 
         // Add inputs to the modal
-        modal.addComponents(primaryActionRow);
+        modal.addComponents(primaryActionRow, secondaryActionRow);
 
         // Show the modal to the user
         await interaction.showModal(modal);
--- modals/deleteBottle.js
+++ modals/deleteBottle.js
@@ -1,4 +1,6 @@
 const bottleDB = require("../database/bottle");
+const messageDB = require("../database/message");
+const createEmbeds = require("../utils/createEmbeds");
 
 module.exports = {
     name: 'deleteBottle',
@@ -6,14 +8,29 @@ module.exports = {
 
         // Check if the user has confirmed the deletion
         const textSuppression = interaction.fields.getTextInputValue('textSuppression');
+        let textRaison = interaction.fields.getTextInputValue('textRaison');
         if (!textSuppression.toLocaleLowerCase().includes('supprimer')) {
             await interaction.reply({ content: 'La bouteille n\'a pas été supprimée.', ephemeral: true });
             return;
         }
 
+        textRaison = textRaison ? textRaison : 'Aucune raison spécifiée';
+
+        await messageDB.insertMessage(interaction.channel.id, interaction.channel.id, interaction.userId, textRaison);
+
         // Send bottle terminated and archived
         await bottleDB.setBottleTerminated(interaction.channel.id);
         await bottleDB.setBottleArchived(interaction.channel.id);
+
+        const receiverId = await bottleDB.getReceiver(interaction.channel.id);
+        const receiver = await interaction.guild.members.fetch(receiverId);
+        if (receiver) {
+            try {
+                const bottle = await bottleDB.getBottle(interaction.channel.id);
+                const embed = createEmbeds.createFullEmbed('Bouteille terminée', 'La ' + bottle.name + ' a été terminée par votre correspondant pour la raison suivante : ' + textRaison, null, null, null, null);
+                await receiver.send({ embeds: [embed] });
+            } catch {}
+        }
 
         // Delete channel
         await interaction.channel.delete();
