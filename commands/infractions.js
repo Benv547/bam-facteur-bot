3c90489a03287470920f94ca5e4d7b66504f448b
Suzoka/Morgan <mor9an77@gmail.com>
2023-02-23 19:12:32 +0100

    fix: /infractions

--- commands/infractions.js
+++ commands/infractions.js
@@ -18,12 +18,17 @@ module.exports = {
 
             let userId = await userDB.getUser(interaction.options.getString('userid'));
             let user;
-
             try {
                 user = await interaction.guild.members.fetch(interaction.options.getString('userid'));
             }
             catch {
                 return interaction.reply({ content: 'Cet utilisateur n\'est pas sur le serveur ou n\'existe pas.', ephemeral: true });
+            }
+
+            if (userId === null) {
+                // Add the user to the database
+                await userDB.createUser(interaction.options.getString('userid'), 0, 0);
+                userId = await userDB.getUser(interaction.options.getString('userid'))
             }
 
             const nbWarnAbus = await sanctionDB.countDetail(userId.id_user, "abusif");
