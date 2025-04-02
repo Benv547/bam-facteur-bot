35690377cee2be23c0e90fd621d40eeac061de54
Benjamin VIGNOT <benv547@device-32.home>
2025-04-02 22:13:36 +0200

    Refactor: update image text rendering with new font and adjust layout parameters

--- utils/images.js
+++ utils/images.js
@@ -12,15 +12,17 @@ const LETTER_WIDTH = DEFAULT_WIDTH - 100;
 const LETTER_HEIGHT = DEFAULT_HEIGHT - 140;
 const LETTER_X = 50;
 const LETTER_Y = 70;
-const LINE_HEIGHT = 22;
-const FONT_SIZE = 16;
+const LINE_HEIGHT = 28;
+const FONT_SIZE = 36;
 
 const DEFAULT_TEXT = "Lorem 🥨 ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan tempus dui, 🥘 eget venenatis elit dignissim id. Suspendisse eleifend venenatis tortor id congue. 🍬 Maecenas sed finibus tellus. Fusce🍬 🍬viverra sagittis libero, quis aliquam tortor facilisis ac. Nulla facilisi. Ut a hendrerit ligula. Curabitur id dolor non ligula egestas scelerisque id non leo. Maecenas commodo, arcu sed suscipit vehicula, velit nunc posuere massa, in imperdiet nunc purus non urna. Aliquam erat volutpat. Nulla id odio non diam malesuada commodo. Nulla id ligula nibh. Cras ultricies rhoncus ipsum id dapibus. Integer ut enim mi.\n" +
     "\n" +
     "Duis ullamcorper faucibus gravida. Aenean rutrum magna semper velit fringilla, in dignissim orci vestibulum. Fusce sit 🍬 amet mauris ligula. Duis lobortis, magna et molestie placerat, nibh tortor tempus erat, vitae varius dui turpis in dui. Suspendisse potenti. Curabitur posuere ullamcorper ipsum quis tincidunt. Nam aliquet bibendum urna. Phasellus id tellus sagittis, feugiat nisi sit amet, luctus justo. Suspendisse eget elit tempus, euismod mauris et, feugiat lectus. Praesent vel viverra massa, et pellentesque ipsum. Maecenas sit amet pulvinar orci, at pellentesque mi. Pellentesque eget mattis odio. Donec rutrum velit sit amet pulvinar posuere. In hac habitasse platea dictumst. Proin porttitor lobortis urna, id iaculis enim mollis eu.";
 
-const FONT_PATH = resolve('fonts', 'open-dyslexic.ttf');
-registerFont(FONT_PATH, { family: 'OpenDyslexic' });
+// const FONT_PATH_DYSLEXIC = resolve('fonts', 'open-dyslexic.ttf');
+// registerFont(FONT_PATH_DYSLEXIC, { family: 'OpenDyslexic' });
+const FONT_PATH = resolve('fonts', 'journal.ttf');
+registerFont(FONT_PATH, { family: 'Journal' });
 console.log('Font loaded !');
 
 function createBaseImage(width, height) {
@@ -36,7 +38,7 @@ function createBaseImage(width, height) {
 function writeTextOnImage(ctx, text, color, x, y, maxWidth) {
     const lines = text.split('\n');
 
-    ctx.font = `${FONT_SIZE}px OpenDyslexic`;
+    ctx.font = `${FONT_SIZE}px Journal`;
     ctx.fillStyle = '#' + color;
 
     for (const line of lines) {
@@ -76,7 +78,7 @@ async function createMyCustomImage(text, color, letterUrl, backgroundUrl) {
         await drawImage(ctx, backgroundUrl, ZERO, ZERO, DEFAULT_WIDTH, DEFAULT_HEIGHT);
         await drawImage(ctx, letterUrl, LETTER_X, LETTER_Y, LETTER_WIDTH, LETTER_HEIGHT);
 
-        writeTextOnImage(ctx, text, color, LETTER_X + 35, LETTER_Y + 70, LETTER_WIDTH - 100);
+        writeTextOnImage(ctx, text, color, LETTER_X + 50, LETTER_Y + 160, LETTER_WIDTH - 105);
 
         return {
             attachment: canvas.toBuffer(),
